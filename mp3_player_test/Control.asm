b940e1b7f2ecc0eee20e1ee43581fefd4097236b
fcpatgithub <fcpatgoo@gmail.com>
2015-04-07 22:17:05 +0800

    reconstruction

---
+++ mp3_player_test/Control.asm
@@ -0,0 +1,90 @@
+.386
+.model flat, stdcall
+option casemap :none   ; case sensitive
+
+
+include		windows.inc
+include		user32.inc
+include		kernel32.inc
+include		comctl32.inc
+include		comdlg32.inc
+include		winmm.inc
+include     msgstruct.inc
+include		masm32rt.inc
+include		Control.inc
+include		Track.inc
+
+includelib	user32.lib
+includelib	kernel32.lib
+includelib	comctl32.lib
+includelib	comdlg32.lib
+includelib	winmm.lib
+
+.data
+extern dwFlag		: DWORD
+extern Pos			: DWORD
+extern hButton		: DWORD
+extern hWinBar		: DWORD
+extern hRect		: DWORD
+extern hPlay		: DWORD
+extern hPause		: DWORD
+
+ButtonClassName BYTE "button", 0
+ButtonText BYTE " ", 0
+barName  BYTE "msctls_trackbar32",0
+barclassName   BYTE "msctls_trackbar32",0
+.code
+
+;********************************************************************
+CreatePlayButton PROC,
+	hWnd: DWORD, hIns: DWORD
+
+	INVOKE CreateWindowEx, NULL, ADDR ButtonClassName,
+		ADDR ButtonText, WS_CHILD or WS_VISIBLE or WS_CLIPCHILDREN or BS_BITMAP,
+		10, 10, 50, 50, hWnd, playBtn_ID, hIns, NULL
+	mov hButton, eax
+	INVOKE LoadImage, hIns, IDB_PLAY, IMAGE_BITMAP, 50, 50, LR_DEFAULTCOLOR
+	mov hPlay, eax
+	INVOKE SendMessage, hButton, BM_SETIMAGE, IMAGE_BITMAP, eax	
+	INVOKE CreateEllipticRgn, 0, 0, 50, 50							
+	mov hRect, eax
+	INVOKE SetWindowRgn, hButton, hRect, TRUE
+	ret
+
+CreatePlayButton ENDP
+;********************************************************************
+CreateTrackBar PROC,
+	hWnd: DWORD, hIns: DWORD
+
+	INVOKE CreateWindowEx, NULL, ADDR barclassName,
+		ADDR barName,WS_CHILD+WS_VISIBLE,70,70,200,20, hWnd,Bar_ID,hIns,NULL
+	mov hWinBar, eax
+	INVOKE SendMessage, hWinBar, TBM_SETPAGESIZE, 0, 1
+	ret
+
+CreateTrackBar ENDP
+;********************************************************************
+SwitchTrackState PROC
+	INVOKE SendMessage, hButton, BM_GETIMAGE, IMAGE_BITMAP, NULL
+	.IF dwFlag == 0 || dwFlag == 2
+		INVOKE SendMessage, hButton, BM_SETIMAGE, IMAGE_BITMAP, hPause
+		call _PlayMP3
+	.ELSE
+		INVOKE SendMessage, hButton, BM_SETIMAGE, IMAGE_BITMAP, hPlay
+		call _PausePlayMP3
+	.ENDIF
+	ret
+SwitchTrackState ENDP
+;********************************************************************
+BarAdjust PROC
+	push eax
+	INVOKE SendMessage, hWinBar, TBM_GETPOS, 0, 0
+	.IF (Pos != eax)
+		mov Pos, eax
+		call _SeekMP3
+	.ENDIF
+	pop eax
+	ret
+BarAdjust ENDP
+;********************************************************************
+END
---
+++ mp3_player_test/Track.asm
@@ -0,0 +1,146 @@
+.386
+.model flat, stdcall
+option casemap :none   ; case sensitive
+
+
+include		windows.inc
+include		user32.inc
+include		kernel32.inc
+include		comctl32.inc
+include		comdlg32.inc
+include		winmm.inc
+include     msgstruct.inc
+include		masm32rt.inc
+include		Track.inc
+
+includelib	user32.lib
+includelib	kernel32.lib
+includelib	comctl32.lib
+includelib	comdlg32.lib
+includelib	winmm.lib
+
+.data
+extern dwFlag			: DWORD
+extern hDevice			: DWORD
+extern szBuffer			: BYTE
+extern stOpenFileName	: OPENFILENAME
+extern hWinMain			: DWORD
+extern Pos				: DWORD
+
+szCaption	BYTE	"Error...",0
+szError		BYTE	"Error to play MP3 file!",0
+szPlay		BYTE	"&Play",0
+szStop		BYTE	"&Stop",0
+szDevice	BYTE	"MPEGVideo",0
+szTitleSave	BYTE	"Open MP3 file...",0
+szExt		BYTE	'*.mp3',0
+szFilter	BYTE	'MP3 Files(*.mp3)',0,'*.mp3',0,'All Files(*.*)',0,'*.*',0,0
+
+.code
+
+;********************************************************************
+_GetFileName	proc
+
+		mov	stOpenFileName.Flags,OFN_PATHMUSTEXIST or OFN_FILEMUSTEXIST
+		mov	stOpenFileName.lStructSize,SIZEOF stOpenFileName
+		mov	eax,hWinMain
+		mov	stOpenFileName.hWndOwner,eax
+		mov	stOpenFileName.lpstrFilter,offset szFilter	;
+		mov	stOpenFileName.lpstrFile,offset szBuffer	;
+		mov	stOpenFileName.nMaxFile,255			;
+		mov	stOpenFileName.lpstrInitialDir,0
+		mov	stOpenFileName.lpstrTitle,offset szTitleSave
+		mov	stOpenFileName.lpstrDefExt,offset szExt
+		invoke	GetOpenFileName,offset stOpenFileName
+		.if	eax == FALSE
+			ret
+		.endif
+		invoke	SetDlgItemText,hWinMain,ID_FILE,addr szBuffer
+		call	_StopPlayMP3
+
+		ret
+
+_GetFileName	endp
+;********************************************************************
+_PlayMP3	proc
+		
+		local	@stMCIPlay:MCI_GENERIC_PARMS
+		local	@stMCIOpen:MCI_OPEN_PARMS
+		mov ebx, dwFlag
+		.if ebx == 0
+			mov	@stMCIOpen.lpstrDeviceType,offset szDevice
+			mov	@stMCIOpen.lpstrElementName,offset szBuffer
+			invoke	mciSendCommand,0,MCI_OPEN,MCI_OPEN_TYPE or MCI_OPEN_ELEMENT,addr @stMCIOpen
+			mov	eax,@stMCIOpen.wDeviceID
+			mov	hDevice,eax
+			mov	eax,hWinMain
+			mov	@stMCIPlay.dwCallback,eax
+			invoke	mciSendCommand,hDevice,MCI_PLAY,MCI_NOTIFY,addr @stMCIPlay
+		.else
+
+			invoke	mciSendCommand,hDevice,MCI_RESUME,MCI_NOTIFY,addr @stMCIPlay
+		.endif
+			
+		.if	eax == 0
+			invoke	SetDlgItemText,hWinMain,IDOK,offset szStop
+			mov	dwFlag, 1
+		.else
+			invoke	MessageBox,hWinMain,addr szError,addr szCaption,MB_OK
+		.endif
+		ret
+        
+_PlayMP3	endp
+;********************************************************************
+_PausePlayMP3	proc
+		local	@stMCIStop:MCI_GENERIC_PARMS
+		
+		mov	eax,hWinMain
+		mov	@stMCIStop.dwCallback,eax
+		invoke	mciSendCommand,hDevice,MCI_PAUSE,MCI_NOTIFY,addr @stMCIStop
+		invoke	SetDlgItemText,hWinMain,IDOK,offset szPlay
+		mov	dwFlag, 2
+		ret
+
+_PausePlayMP3	endp
+;********************************************************************
+_StopPlayMP3	proc
+		local	@stMCIStop:MCI_GENERIC_PARMS
+		
+		mov	eax,hWinMain
+		mov	@stMCIStop.dwCallback,eax
+		invoke	mciSendCommand,hDevice,MCI_STOP,MCI_NOTIFY,addr @stMCIStop
+		invoke	mciSendCommand,hDevice,MCI_CLOSE,MCI_NOTIFY,addr @stMCIStop
+		invoke	SetDlgItemText,hWinMain,IDOK,offset szPlay
+		mov	dwFlag, 0
+		ret
+
+_StopPlayMP3	endp
+;********************************************************************
+_SeekMP3	proc
+		local	@stMCIStatus:MCI_STATUS_PARMS
+		local	@stMCISeek:MCI_SEEK_PARMS
+		local	@stMCIPlay:MCI_GENERIC_PARMS
+		mov ebx, dwFlag
+		.if (ebx == 1)
+			mov eax, MCI_STATUS_LENGTH
+			mov  @stMCIStatus.dwItem, eax
+			mov	eax,hWinMain
+			mov	@stMCIStatus.dwCallback,eax
+			invoke	mciSendCommand,hDevice,MCI_STATUS,MCI_STATUS_ITEM,addr @stMCIStatus
+			push ecx
+			mov eax, @stMCIStatus.dwReturn
+			imul Pos
+			mov ecx, 100
+			div ecx
+			pop ecx
+			mov @stMCISeek.dwTo, eax
+			mov	eax,hWinMain
+			mov	@stMCISeek.dwCallback,eax
+			invoke	mciSendCommand,hDevice,MCI_SEEK,MCI_TO+MCI_WAIT,addr @stMCISeek
+			invoke	mciSendCommand,hDevice,MCI_PLAY,MCI_NOTIFY,addr @stMCIPlay
+		.endif
+		ret
+        
+_SeekMP3	endp
+;********************************************************************
+END
